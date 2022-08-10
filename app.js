a60054dc6acbf3aed2bb71dd71bb42e05277e5ec
zaeisan <108772332+zaeisan@users.noreply.github.com>
2022-08-10 16:53:58 -0500

    Subo el proyecto

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log('Servidor funcionando');
+});
